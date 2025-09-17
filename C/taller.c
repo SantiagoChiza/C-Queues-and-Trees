60264a06bf91b5723444b7e84822766139b701e8
Santiago Chiquiza <eschiquizac@academia.usbbog.edu.co>
2025-09-17 21:26:18 +0000

    Terminado

--- C/taller.c
+++ C/taller.c
@@ -25,11 +25,75 @@ void ejercicio4() {
     }
     printf("Ejercicio 4: Suma de elementos = %d\n", suma);
 }
+struct Persona {
+    char nombre[20];
+    int edad;
+};
+
+void ejercicio5() {
+    struct Persona p1 = {"Ana", 30};
+    struct Persona *ptr = &p1;
+    printf("Ejercicio 5: %s tiene %d años\n", ptr->nombre, ptr->edad);
+}
+void ejercicio6() {
+    int x = 7;
+    int *p = &x;
+    int **pp = &p;
+    printf("Ejercicio 6: x = %d, *p = %d, **pp = %d\n", x, *p, **pp);
+}
+void incrementar(int *n) {
+    (*n)++;
+}
+
+void ejercicio7() {
+    int x = 10;
+    incrementar(&x);
+    printf("Ejercicio 7: x incrementado = %d\n", x);
+}
+void ejercicio8() {
+    int *p = (int*)malloc(sizeof(int) * 3);
+    if (p != NULL) {
+        p[0] = 100; p[1] = 200; p[2] = 300;
+        printf("Ejercicio 8: %d %d %d\n", p[0], p[1], p[2]);
+        free(p);
+    }
+}
+void ejercicio9() {
+    char *nombres[3] = {"Ana", "Luis", "Marta"};
+    printf("Ejercicio 9: ");
+    for(int i = 0; i < 3; i++) {
+        printf("%s ", nombres[i]);
+    }
+    printf("\n");
+}
+int suma(int a, int b) { return a + b; }
+int resta(int a, int b) { return a - b; }
+
+void ejercicio10() {
+    int (*operacion)(int, int);
+    operacion = suma;
+    printf("Ejercicio 10: Suma = %d\n", operacion(2, 3));
+    operacion = resta;
+    printf("ejercicio 10: Resta = %d\n", operacion(5, 2));
+}
+
+
+
+
+
+
 
 int main() {
     ejercicio1();
     ejercicio2();
     ejercicio3();
     ejercicio4();
+    ejercicio5();
+    ejercicio6();
+    ejercicio7();
+    ejercicio8();
+    ejercicio9();
+    ejercicio10();
+    
     return 0;
 }
